b9fdd6b32cd2ebdec794d647563d60345f92c3ea
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2024-06-14 11:44:05 +0200

    add: added method of class ContoBancario.java aka SNACK2

--- src/main/java/org/lessons/java/oop/Main.java
+++ src/main/java/org/lessons/java/oop/Main.java
@@ -7,8 +7,17 @@ public class Main {
 		
 		//SNACK 1
 		Studente students = new Studente("Luca", "Novelli", "23");
-		System.out.println(students.getConcatenation());
+		System.out.println("---------- SNACK 1 --------");
+		System.out.println("- CONCATENAZIONE ---> " + students.getConcatenation());
+		System.out.println("");
 		
+		//SNACK 2
+		System.out.println("---------- SNACK 2 --------");
+		ContoBancario bankAccountPersonal = new ContoBancario("IT 12 A 12345 12345 123456789012");
+		bankAccountPersonal.setBalance(11.000);
+		System.out.printf("- SALDO DISPONIBILE ---> %.3f€ \n",bankAccountPersonal.getBalance());
+		bankAccountPersonal.setLessBalance(5.000);
+		System.out.printf("- SALDO DISPONIBILE ---> %.3f€ \n",bankAccountPersonal.getBalance());
 	}
 
 }
