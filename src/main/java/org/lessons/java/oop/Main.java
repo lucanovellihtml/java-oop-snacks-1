ff8177b3798b06e8859b709e2cc55e12275331ec
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2024-06-14 10:49:37 +0200

    add: added method of class Studente.java aka SNACK1

---
+++ src/main/java/org/lessons/java/oop/Main.java
@@ -0,0 +1,14 @@
+package org.lessons.java.oop;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//SNACK 1
+		Studente students = new Studente("Luca", "Novelli", "23");
+		System.out.println(students.getConcatenation());
+		
+	}
+
+}
