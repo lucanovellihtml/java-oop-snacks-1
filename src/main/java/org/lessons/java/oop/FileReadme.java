e66bae5333765af2aa5c8d219dcc5635f82ac93d
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2024-07-29 10:38:17 +0200

    add: add request bonus
    I added custom exception

--- src/main/java/org/lessons/java/oop/FileReadme.java
+++ src/main/java/org/lessons/java/oop/FileReadme.java
@@ -1,20 +1,27 @@
 package org.lessons.java.oop;
 
 import java.io.File; // CLASSE PER L'ACQUISIZIONE DEL PATH DEL FILE;
+
+//CLASSE PER LA GESTIONE DELL'ECCEZIONE;
 import java.io.FileNotFoundException;
 import java.util.NoSuchElementException;
+
 import java.util.Scanner; // CLASSE PER SCANNERIZZARE IL CONTENUTO DEL FILE;
+
 
 public class FileReadme {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws FileNotFoundException {
+
+		// CREAZIONE OGGETTO FILE
 		File fileTxt = new File("readme.txt");
 		
 		try {
 			
+			// CREAZIONE OGGETTO SCANNER CHE ANDRA' A LEGGERE IL CONTENUTO DEL FILE
 			Scanner readLineFile = new Scanner(fileTxt);
 			
+			// VIENE CONTROLLATO SE , AL CICLARE DELLA NUOVA RIGA, ESISTE UN'ALTRA RIGA E VIENE STAMPATA;
 			while(readLineFile.hasNextLine()) {
 				String line = readLineFile.nextLine();
 				System.out.println(line);		
@@ -23,15 +30,22 @@ public class FileReadme {
 		readLineFile.close();
 		System.out.println("ESECUZIONE FINITA");
 			
+		// GESTIONE DELL'ECCEZIONE SE IL FILE NON E' TROVATO;
 		} catch (FileNotFoundException e) {
 			
 			System.out.println("ERROR FILE NOT FOUND ---> " + e.getMessage());
-	
+			
+			//BONUS;
+			throw new FileNotFoundException ("ERROR FILE NOT FOUND ---> IL FILE NON E' PRESENTE");
 		}	
 		
+		// GESTIONE DELL'ECCEZIONE SE IL FILE NON PRESENTA CONTENUTO;
 		catch (NoSuchElementException  e) {
-			
+		
 			System.out.println("ERROR NO SUCH ELEMENT ---> " + e.getMessage());
+			
+			//BONUS;
+			throw new NoSuchElementException ("ERROR NO SUCH ELEMENT ---> IL FILE NON PRESENTA CONTENUTO");
 			
 		}
 		
